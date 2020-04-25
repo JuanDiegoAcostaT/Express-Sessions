ae7d1f810a77cdc3e72b3963f5c5f2dd192bc19a
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-04-25 16:00:53 -0500

    First Renderizing File ejs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middlewares
 
+
+
 // Routes
+app.use(require('./routes/index'))
+
 
 // Static Files
 
